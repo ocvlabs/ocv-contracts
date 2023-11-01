2e78c96994e168dc553ee0c53bb04abb48264221
raldblox <raldblox@outlook.com>
2023-11-01 20:22:25 +0800

    updated interfaces

--- contracts/interfaces/IInteractiveAsset.sol
+++ contracts/interfaces/IInteractiveAsset.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+/// @title Allow contracts to mintAsset at assetFactory
+/// @author rald@ocvlabs.com
 interface IInteractiveAsset {
     function viewAssetName() external view returns (string memory);
 
